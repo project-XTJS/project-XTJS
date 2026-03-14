e00f8a602af43ec58a8953b57543543ab2a45160
ghb-art <2849415298@qq.com>
2026-03-14 15:18:46 +0800

    refactor：数据库表重构
    Signed-off-by: ghb-art <2849415298@qq.com>

--- db/migration/V20260306145302__create_xtjs_documents.sql
+++ db/migration/V20260306145302__create_xtjs_documents.sql
@@ -4,6 +4,8 @@ CREATE TABLE xtjs_documents
     identifier_id VARCHAR(64) NOT NULL,
     file_name     TEXT        NOT NULL,
     file_url      TEXT        NOT NULL,
+    extracted     BOOLEAN     NOT NULL DEFAULT FALSE,
+    content       JSONB,
     deleted       BOOLEAN     NOT NULL DEFAULT FALSE,
     create_time   TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
     update_time   TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -20,6 +22,10 @@ COMMENT
 ON COLUMN xtjs_documents.file_name IS '文件名称';
 COMMENT
 ON COLUMN xtjs_documents.file_url IS '文件URL（MinIO存储地址）';
+COMMENT
+ON COLUMN xtjs_documents.extracted IS '是否已识别：false-未识别，true-已识别';
+COMMENT
+ON COLUMN xtjs_documents.content IS 'JSON格式的文件识别结果';
 COMMENT
 ON COLUMN xtjs_documents.deleted IS '逻辑删除：false-未删除，true-已删除';
 COMMENT
