7d89c7485791e4e36859d806202e58c6cab3cc16
ghb-art <2849415298@qq.com>
2026-03-15 17:22:04 +0800

    refactor：使用统一桶名update_file
    Signed-off-by: ghb-art <2849415298@qq.com>

--- app/config/minio.py
+++ app/config/minio.py
@@ -8,7 +8,8 @@ class MinioConfig:
     ENDPOINT = os.getenv("MINIO_ENDPOINT", "127.0.0.1:9000")
     ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "minioadmin")
     SECRET_KEY = os.getenv("MINIO_SECRET_KEY", "minioadmin")
-    BUCKET_NAME = os.getenv("MINIO_BUCKET_NAME", "update_file")
+    # Fixed business bucket: do not auto-switch to any other bucket.
+    BUCKET_NAME = "update_file"
     SECURE = os.getenv("MINIO_SECURE", "false").lower() == "true"
     URL_EXPIRE_DAYS = int(os.getenv("MINIO_PRESIGNED_EXPIRES_DAYS", "7"))
     MAX_FILE_SIZE = int(os.getenv("MINIO_MAX_FILE_SIZE", str(500 * 1024 * 1024)))
